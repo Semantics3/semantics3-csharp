9017bdcb45cab43fd4f5e114519fef247b2543f0
mcdand2 <mcdand2@LPTMCDAND2>
2018-03-28 11:27:15 -0400

    Converted to .NETStandard2.0 With Dual Compile to NET45

--- Src/Semantics3/Properties/AssemblyInfo.cs
+++ Src/Semantics3/Properties/AssemblyInfo.cs
@@ -1,72 +1,72 @@
-﻿#region License
-/******************************************************************************
- * $Id: $
- * 
- * Name:     Semantics3.cs
- * Project:  Semantics3 API C# Client
- * Purpose:  C# Client to query Semantics3 APIs.
- * $Author:   Srinivasan Kidambi, srinivas@semantics3.com
- *
- ******************************************************************************/
+﻿//#region License
+///******************************************************************************
+// * $Id: $
+// * 
+// * Name:     Semantics3.cs
+// * Project:  Semantics3 API C# Client
+// * Purpose:  C# Client to query Semantics3 APIs.
+// * $Author:   Srinivasan Kidambi, srinivas@semantics3.com
+// *
+// ******************************************************************************/
 
-//The MIT License
+////The MIT License
 
-//Copyright (c) 2013 Semantics3, Inc. https://semantics3.com
+////Copyright (c) 2013 Semantics3, Inc. https://semantics3.com
 
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this software and associated documentation files (the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//copies of the Software, and to permit persons to whom the Software is
-//furnished to do so, subject to the following conditions:
+////Permission is hereby granted, free of charge, to any person obtaining a copy
+////of this software and associated documentation files (the "Software"), to deal
+////in the Software without restriction, including without limitation the rights
+////to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+////copies of the Software, and to permit persons to whom the Software is
+////furnished to do so, subject to the following conditions:
 
-//The above copyright notice and this permission notice shall be included in
-//all copies or substantial portions of the Software.
+////The above copyright notice and this permission notice shall be included in
+////all copies or substantial portions of the Software.
 
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//THE SOFTWARE.
-#endregion
+////THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+////IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+////FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+////AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+////LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+////OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+////THE SOFTWARE.
+//#endregion
 
-using System;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+//using System;
+//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: CLSCompliant(true)]
-[assembly: AssemblyTitle("Semantics3 API C# Library")]
-[assembly: AssemblyDescription("C# Client Library for querying Semantics3 APIs")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Semantics3")]
-[assembly: AssemblyProduct("Semantics3 API C# Client")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: CLSCompliant(true)]
+//[assembly: AssemblyTitle("Semantics3 API C# Library")]
+//[assembly: AssemblyDescription("C# Client Library for querying Semantics3 APIs")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Semantics3")]
+//[assembly: AssemblyProduct("Semantics3 API C# Client")]
+//[assembly: AssemblyCopyright("Copyright ©  2013")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b293707-9b13-45df-aa49-605beffb409e")]
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("3b293707-9b13-45df-aa49-605beffb409e")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.27")]
-[assembly: AssemblyFileVersion("1.0.0.27")]
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//// You can specify all the values or you can default the Build and Revision Numbers 
+//// by using the '*' as shown below:
+//// [assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.27")]
+//[assembly: AssemblyFileVersion("1.0.0.27")]
